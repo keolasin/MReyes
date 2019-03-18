e5bfe881cb78ef03edc24fd041862774dfc74f04
Keolasin <43076895+keolasin@users.noreply.github.com>
2019-03-17 20:44:11 -0700

    image backgrounds add

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,27 +3,38 @@ import React, { Component } from 'react';
 class Landing extends Component {
   constructor(props){
     super(props);
+
+    this.state={
+      isHovered: false
+    }
+  }
+
+  handleHover(){
+
   }
 
   render(){
     return(
       <section className='landing-bin'>
         <article className='triple education'>
-          <img href='./../../imgs/dog_vert.jpg' />
+          <img src='./assets/images/dog_vert.jpg'
+               alt='Ocean Beach with dog'/>
           <section className='hover-container'>
-            <a id='education'>Click me</a>
+            <a id='education'></a>
           </section>
         </article>
 
         <article className='triple about'>
-          <img href='./../../imgs/bike_vert.jpg' />
+          <img src='./assets/images/bike_vert.jpg'
+               alt='Matthew on bike smiling'/>
           <section className='hover-container'>
             <a id='about'></a>
           </section>
         </article>
 
         <article className='triple portfolio'>
-          <img href='./../../imgs/run_vert.jpg' />
+          <img src='./assets/images/run_vert.jpg'
+               alt='Green mountainside with running trail'/>
           <section className='hover-container'>
             <a id='portfolio'></a>
           </section>
